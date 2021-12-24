418fc0e5b96defe87f0464ecf7a553af2ff401cf
SAMEERNOORISAWSOME <96590024+SAMEERNOORISAWSOME@users.noreply.github.com>
2021-12-24 19:02:13 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    //update the quiz
+    update(quiz) 
+      database.ref("/").update({
+        gameState: quiz
+      });
+  }
+  if(gameState === 1){
+    clear();
+    //update the play 
+    update(play) 
+      database.ref("/").update({
+        gameState: play
+      });
+    play.update();
+  }
+}
